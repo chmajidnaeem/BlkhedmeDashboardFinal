aedf07c8e9a562eaad0aef676f61bf061fb87033
Ariba Memon <76126161+Ariba-memon@users.noreply.github.com>
2024-09-11 23:55:58 +0500

    resolvedconflicts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     "./src/**/*.{js,jsx,ts,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
+
 
  /** @type {import('tailwindcss').Config} */
-=======
+
  
-  // @type {import('tailwindcss').Config} 
->>>>>>> 6bdd2ffb17a09a2da77b6a09a0c28b3425529527
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
