824a439debea9b88ad04e6e6146e5c56c8f3a23a
Ariba Memon <76126161+Ariba-memon@users.noreply.github.com>
2024-09-11 23:44:15 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,4 @@
-import './App.css';
-<<<<<<< HEAD
-import Home from './Components/Home';
-// import { Router } from 'react-router-dom';
-import Router from "../src/Components/Router";
 
-function App() {
-  return (
-    <div>
-     {/* <Home/> */}
-     <Router/>
-    </div>
-=======
 import LoginPage from './pages/loginPage/LoginPage';
 
 function App() {
@@ -18,7 +6,7 @@ function App() {
   <div className='w-full h-screen max-w-screen-2xl mx-auto overflow-x-hidden'>
      <LoginPage />
   </div>
->>>>>>> 6bdd2ffb17a09a2da77b6a09a0c28b3425529527
+
   );
 }
 
